7a6061426ef7311fd8ebac4c96f56e71cc35d70e
Brandon Jimenez <jimenezchronic@yahoo.com>
2014-09-17 13:58:44 -0700

    made predictions

---
+++ app/src/main/java/android/jimenezb/crystalball/Predictions.java
@@ -0,0 +1,21 @@
+package android.jimenezb.crystalball;
+
+public class Predictions {
+
+    private static Predictions predictions;
+    private String[] answers;
+
+    private Predictions(){
+        answers = new String[]{
+          "your milk is spoiled!"
+        };
+    }
+
+    public static Predictions get(){
+        if(predictions == null){
+            predictions = new Predictions();
+        }
+        return  predictions;
+    }
+
+}
